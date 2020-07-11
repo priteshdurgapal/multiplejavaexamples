e4f330f8f5c10ddccb18dabdb14698e14545b260
Pritesh <pritesh19@live.in>
2020-07-11 21:41:23 +0530

    refactored

--- com/check/exc/CustomException.java
+++ com/check/exc/CustomException.java
@@ -1,12 +1,12 @@
 package com.check.exc;
 
-public class CustomException extends Exception{
+public class CustomException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	public CustomException(String message) {
 		super(message);
 	}
